0e3ffce08c6e14f390c92b7b65a257345dc948d3
Proftoa <ogunjobimoshoodo@gmail.com>
2023-03-14 00:41:32 -0400

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 let firstArg = process.argv[2];
 if (!firstArg || isNaN(firstArg)) {
-  console.log('Not a number');
+  console.log('Missing size');
 } else {
   firstArg = parseInt(firstArg);
-  console.log('My number: ' + firstArg);
+  for (let i = 0; i < firstArg; i++) {
+    console.log('X'.repeat(firstArg));
+  }
 }
