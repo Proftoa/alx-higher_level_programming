fe7e434b61c09605275fde5bb809e597d35de01c
Proftoa <ogunjobimoshoodo@gmail.com>
2023-03-14 05:23:18 -0400

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const args = process.argv;
+/*const args = process.argv;
 
 if (args[2]) {
   console.log(args[2];
 } else {
   console.log('No argument');
-}
+}*/
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
